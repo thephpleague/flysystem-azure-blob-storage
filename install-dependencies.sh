2623958eab743e6402ed41ab8e9010a2441d6f56
Florent Viel <luxifer666@gmail.com>
2018-03-20 15:18:17 +0100

    improve composer install command

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -3,6 +3,6 @@
 set -e
 
 docker run -e executable=blob --name azureblob -d -t -p 10000:10000 arafato/azurite
-composer install --prefer-dist
+composer install --prefer-dist --no-progress --no-interaction
 sleep 15
 curl -X PUT -H "$x_ms_date_h" -H "$x_ms_version_h" -H "Content-Length: 0" -H "$authorization_header" "http://0.0.0.0:10000/devstoreaccount1/mycontainer?restype=container"
